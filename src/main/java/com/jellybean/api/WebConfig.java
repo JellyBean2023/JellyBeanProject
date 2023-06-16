7ebc39bd7b2d464d9220722213601368fc308def
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-06-16 17:40:34 +0900

    repository test

--- src/main/java/com/jellybean/api/WebConfig.java
+++ src/main/java/com/jellybean/api/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.jellybean.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
