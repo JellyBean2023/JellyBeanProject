e5f46fd8f4271283b75785770a4fe1d41704f60d
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-06-16 17:02:58 +0900

    Create JellyBeanProject Repository

---
+++ src/main/java/com/jellybean/api/JellybeanApplication.java
@@ -0,0 +1,13 @@
+package com.jellybean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellybeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellybeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jellybean/api/JellybeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellybean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellybeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
