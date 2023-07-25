b129803b2b8c2f1f04794f912f7a33b315d83315
ekgml4573 <dh4573@naver.com>
2023-07-25 20:52:17 +0900

    풀 병합 후 수정

--- src/main/java/com/jellybean/api/controller/EmailController.java
+++ src/main/java/com/jellybean/api/controller/EmailController.java
@@ -2,7 +2,6 @@ package com.jellybean.api.controller;
 
 
 import com.jellybean.api.Vo.EmaillRequest;
-import com.jellybean.api.Vo.UserRequest;
 import com.jellybean.api.service.EmailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,35 +22,6 @@ public class EmailController {
     @Autowired
     private final EmailServiceImpl emailService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<Boolean> signup(@RequestBody UserRequest userRequest) {
-
-        //이메일 중복 확인
-        boolean isEmailDuplicated = memberService.isEmailDuplicated(userRequest.getEmail());
-        if(isEmailDuplicated){
-            return ResponseEntity.badRequest().body(false);
-        }
-
-        //회원가입 성공 처리
-        MemberResponse response = authService.signup(userRequest);
-        return ResponseEntity.ok(true);
-//        return ResponseEntity.ok(authService.signup(memberRequest));
-    }
-
-    //    @PostMapping("/signup")
-//    public ResponseEntity<MemberResponse> signup(@RequestBody MemberRequest memberRequest) {
-//
-//        //이메일 중복 확인
-//        boolean isEmailDuplicated = memberService.isEmailDuplicated(memberRequest.getEmail());
-//        if(isEmailDuplicated){
-//            return ResponseEntity.badRequest().body(new MemberResponse("false"));
-//        }
-//
-//        //회원가입 성공 처리
-//        MemberResponse response = authService.signup(memberRequest);
-//        return ResponseEntity.ok(response);
-////        return ResponseEntity.ok(authService.signup(memberRequest));
-//    }
 
     @PostMapping("/regist/emailConfirm")
     public String emailConfirm(@RequestParam String email) throws Exception {
